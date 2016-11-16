ea1b1ddcc9f7861a1286a61a265a9d1e0baa0435
facchiR <utente@NBCHIWYL0012>
2016-11-16 10:49:38 +0100

    Show 3 students

---
+++ classes/DigitalUser.php
@@ -0,0 +1,10 @@
+<?php
+trait DigitalUser{
+    private $email;
+    public function getEmail(){
+        return $this->email;
+    }
+    public function setEmail($email){
+        $this->email = $email;
+    }    
+}
--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
-echo "Hello world!! :) ";
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require_once 'classes/Student.php';
 
+echo "<h1>Hello world!</h1>";
+
+$student1 = new Student("Donald Duck", 37, "gkgjh@lll");
+$student2 = new Student("Mickey Mouse", 48, "gkgjh@lll");
+$student3 = new Student("Daisy Duck", 34, "gkgjh@lll");
+
+echo $student1;
+echo "<br>";
+echo $student2;
+echo "<br>";
+echo $student3;
